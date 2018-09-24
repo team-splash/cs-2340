1f23d405768ce8c3e24f4dac2ad089ed8c259358
Kaci Hernandez Kluesner <kacihernandezkluesner@lawn-128-61-51-69.lawn.gatech.edu>
2018-09-23 20:14:40 -0400

    everything for m4 is being committed.

---
+++ app/src/main/java/com/example/kacihernandezkluesner/m4/NextActivity.java
@@ -0,0 +1,79 @@
+package com.example.kacihernandezkluesner.m4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Handler;
+import android.os.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+
+        Button buttonLogout = (Button) findViewById(R.id.btn_logout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Message myMessage = new Message();
+                myMessage.obj = "NOTSUCCESS";
+                handler.sendMessage(myMessage);
+                finish();
+            }
+        });
+    }
+
+
+
+
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            String loginmsg=(String)msg.obj;
+            if(loginmsg.equals("NOTSUCCESS")) {
+                Intent intent = new Intent(NextActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+                removeDialog(0);
+            }
+        }
+    };
+}
