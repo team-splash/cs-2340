629fff8fc67cba92a42a150795563372dfcb39db
Matthew Marting <m5w@users.noreply.github.com>
2018-11-16 15:50:22 -0500

    Add test for Locations.getNames

--- donationTracker/app/src/androidTest/java/com/example/teamsplash/donationtracker/kacitest.java
+++ donationTracker/app/src/androidTest/java/com/example/teamsplash/donationtracker/kacitest.java
@@ -1,65 +1,35 @@
 package com.example.teamsplash.donationtracker;
+
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import com.example.teamsplash.donationtracker.model.Location;
 import com.example.teamsplash.donationtracker.model.LocationType;
+import com.example.teamsplash.donationtracker.model.Locations;
 
 import junit.framework.TestCase;
 
-import static com.example.teamsplash.donationtracker.model.LocationType.DR;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
 
 /**
  * kaci's junit tests to test out location objects
  */
 @RunWith(AndroidJUnit4.class)
 public class kacitest extends TestCase{
-    /**
-     * test to see equals method works in the location class
-     */
     @Test
-    public void assertEqual() {
-        Location w = new Location("kaci", DR, 0.00, 0.00,
-                " 47 17th", "atlanta", "Georgia",
-                "30363", "6780000000");
-        Location x = new Location("kaci", DR, 0.00, 0.00,
-                " 47 17th", "atlanta", "Georgia",
-                "30363", "6780000000");
-        boolean y = x.equals(w);
-        assertTrue(y);
-    }
-
-    /**
-     * checks to see if getname method for location works
-     */
-    @Test
-    public void getNames(){
-        Location w = new Location("kaci", DR, 0.00, 0.00,
-                " 47 17th", "atlanta", "Georgia",
-                "30363", "6780000000");
-        Location x = new Location("kaci", DR, 0.00, 0.00,
-                " 47 17th", "atlanta", "Georgia",
-                "30363", "6780000000");
-        String nameW = w.getName();
-        String nameX = x.getName();
-        assertEquals(nameW, nameX);
-    }
-
-    /**
-     * checks to see if getnlocationtype method for location works
-     */
-    @Test
-    public void getLocationTypes(){
-        Location w = new Location("kaci", DR, 0.00, 0.00,
-                " 47 17th", "atlanta", "Georgia",
-                "30363", "6780000000");
-        Location x = new Location("kaci", DR, 0.00, 0.00,
-                " 47 17th", "atlanta", "Georgia",
-                "30363", "6780000000");
-        LocationType LTW = w.getLocationType();
-        LocationType LTX = x.getLocationType();
-        assertEquals(LTW, LTX);
+    public void testLocationsGetNames() {
+        Location a = new Location("A", LocationType.DR, 0.0, 0.0, "A", "A", "MA", "01001", "0000000000");
+        Location b = new Location("B", LocationType.DR, 0.0, 0.0, "B", "B", "MA", "01001", "0000000001");
+        String[] emptyLocationNames = {};
+        String[] aLocationNames = {"A"};
+        String[] abLocationNames = {"A", "B"};
+        assertEquals(Arrays.asList(emptyLocationNames), Locations.getInstance().getNames());
+        Locations.getInstance().add(a);
+        assertEquals(Arrays.asList(aLocationNames), Locations.getInstance().getNames());
+        Locations.getInstance().add(b);
+        assertEquals(Arrays.asList(abLocationNames), Locations.getInstance().getNames());
     }
 }
 
