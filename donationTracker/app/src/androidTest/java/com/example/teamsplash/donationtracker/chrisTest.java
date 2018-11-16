70621dcaecf46b9f5193af5c2fa8ef763750d972
Matthew Marting <m5w@users.noreply.github.com>
2018-11-16 15:28:13 -0500

    Add tests for User and Users

--- donationTracker/app/src/androidTest/java/com/example/teamsplash/donationtracker/chrisTest.java
+++ donationTracker/app/src/androidTest/java/com/example/teamsplash/donationtracker/chrisTest.java
@@ -1,17 +1,16 @@
 package com.example.teamsplash.donationtracker;
+
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.teamsplash.donationtracker.model.Location;
 import com.example.teamsplash.donationtracker.model.LocationType;
-
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-
+import com.example.teamsplash.donationtracker.model.User;
+import com.example.teamsplash.donationtracker.model.UserType;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static com.example.teamsplash.donationtracker.model.LocationType.DR;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -22,27 +21,16 @@ import static org.junit.Assert.assertThat;
 @RunWith(AndroidJUnit4.class)
 public class chrisTest extends TestCase {
     @Test
-    public void LocationsTypeFromString(){
-        Location newLoc = new Location("loc", DR, 1.23, 1.23,
-                "123", "123", "123", "12345", "1234567890");
-        LocationType type = newLoc.getLocationType();
-        LocationType x = LocationType.fromString(type.toString());
-        assertNotNull(x);
-    }
-    @Test
-    public void getTypeMethod() {
-        Location newLoc = new Location("loc", DR, 1.23, 1.23,
-                "123", "123", "123", "12345", "1234567890");
-        LocationType type = newLoc.getLocationType();
-        String theType = type.getType();
-        assertThat(theType, containsString("Drop Off"));
-    }
-    @Test
-    public void getTypeToString() {
-        Location newLoc = new Location("loc", DR, 1.23, 1.23,
-                "123", "123", "123", "12345", "1234567890");
-        LocationType type = newLoc.getLocationType();
-        String theType = type.toString();
-        assertNotNull(theType);
+    public void userEqualsTest() {
+        User a = new User("A", "A", "a@example.com", "aaaaaaaa", UserType.USER);
+        assertFalse(a.equals(null));
+        assertFalse(a.equals(new Object()));
+        assertFalse(a.equals(new User("B", "A", "a@example.com", "aaaaaaaa", UserType.USER)));
+        assertFalse(a.equals(new User("A", "B", "a@example.com", "aaaaaaaa", UserType.USER)));
+        assertFalse(a.equals(new User("A", "A", "b@example.com", "aaaaaaaa", UserType.USER)));
+        assertFalse(a.equals(new User("A", "A", "a@example.com", "bbbbbbbb", UserType.USER)));
+        assertFalse(a.equals(new User("A", "A", "a@example.com", "aaaaaaaa", UserType.LOCATION_EMPLOYEE)));
+        assertTrue(a.equals(new User("A", "A", "a@example.com", "aaaaaaaa", UserType.USER)));
+        assertTrue(a.equals(a));
     }
 }
--- donationTracker/app/src/androidTest/java/com/example/teamsplash/donationtracker/matthewTest.java
+++ donationTracker/app/src/androidTest/java/com/example/teamsplash/donationtracker/matthewTest.java
@@ -2,51 +2,30 @@ package com.example.teamsplash.donationtracker;
 
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.teamsplash.donationtracker.model.Item;
-import com.example.teamsplash.donationtracker.model.Location;
 import com.example.teamsplash.donationtracker.model.User;
+import com.example.teamsplash.donationtracker.model.UserType;
+import com.example.teamsplash.donationtracker.model.Users;
 
+import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 
-
-import junit.framework.TestCase;
-
-
-
-
-import static com.example.teamsplash.donationtracker.model.ItemType.CLO;
-
-import static com.example.teamsplash.donationtracker.model.LocationType.WA;
-
-import static com.example.teamsplash.donationtracker.model.UserType.USER;
-
-
 @SuppressWarnings("ALL")
 @RunWith(AndroidJUnit4.class)
-public class matthewTest extends TestCase{
+public class matthewTest extends TestCase {
     @Test
-    public void userTypeCheck() {
-        User matthew = new User("Matthew", "Marting",
-                "mmarting1@outlook.com", "password123!", USER);
-        assertEquals(matthew.getUserType().toString(), USER.toString());
-    }
-    @Test
-    public void locationTypeCheck() {
-        Location warehouse = new Location("warehouse", WA, -10.00,
-                100.00, "Address", "city", "state",
-                "100000", "1234567890");
-        assertEquals(warehouse.getLocationType().toString(), WA.toString());
-    }
-    @Test
-    public void itemTypeCheck() {
-        Location warehouse = new Location("warehouse", WA, -10.00, 100.00,
-                "Address", "city", "state",
-                "100000", "1234567890");
-        Item sweater = new Item("15:30", warehouse, "sweater",
-                "knitted", 30.00, CLO);
-        assertEquals(sweater.getItemType().toString(), CLO.toString());
+    public void usersAddCheck() {
+        User a = new User("A", "A", "a@example.com", "aaaaaaaa", UserType.USER);
+        User b = new User("B", "A", "a@example.com", "bbbbbbbb", UserType.USER);
+        Users.getInstance().add(a);
+        assertTrue(Users.getInstance().contains("a@example.com", "aaaaaaaa"));
+        assertTrue(Users.getInstance().get("a@example.com", "aaaaaaaa").equals(a));
+        assertFalse(Users.getInstance().contains("a@xample.com", "bbbbbbbb"));
+        Users.getInstance().add(b);
+        assertTrue(Users.getInstance().contains("a@example.com", "aaaaaaaa"));
+        assertTrue(Users.getInstance().get("a@example.com", "aaaaaaaa").equals(a));
+        assertFalse(Users.getInstance().contains("a@xample.com", "bbbbbbbb"));
     }
 }
